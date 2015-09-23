23f4458384b9dac751a136954420bae377c2d19d
server <you@example.com>
2015-09-23 11:12:58 -0300

    Se agrega a las configuraciones una apcion para centralizar los contenidos estaticos, hay una forma para automaticamente recolectar todo. En el wsgi se agrega al enviriorment el path de la carpeta del proyecto para poder incluir bien los otros archivos de python. se modifia el ignore para no copiar sqlite

--- server/wsgi.py
+++ server/wsgi.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
+# add the path to the folder
+sys.path.append('/home/server/Documentos/server')
 
 from django.core.wsgi import get_wsgi_application
 
