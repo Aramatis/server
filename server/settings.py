5e807821e8af2fcd16112306e25513b23ba4425e
roberto <you@example.com>
2015-09-24 18:21:36 -0300

    Map location is added

--- server/settings.py
+++ server/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'AndroidRequests',
+    'MapLocationOfUsers',
 )
 
 MIDDLEWARE_CLASSES = (
--- server/urls.py
+++ server/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^android/', include('AndroidRequests.urls')),
+    url(r'^map/', include('MapLocationOfUsers.urls')),
 ]
