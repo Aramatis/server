4aa71e5db4d1f1344f99087d01674aabc238c54e
roberto <you@example.com>
2015-09-29 17:04:39 -0300

    the markers are added and requestes every ten segund. the older are remove. A new view is make to response to the get active users, and the respective url is added

--- MapLocationOfUsers/urls.py
+++ MapLocationOfUsers/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from MapLocationOfUsers.views import MapHandler
+from MapLocationOfUsers.views import MapHandler, GetMapPositions
 
 urlpatterns = [
 	url(r'^show$', MapHandler.as_view()),
+	url(r'^activeuserpose$', GetMapPositions.as_view()),
 ]
--- MapLocationOfUsers/views.py
+++ MapLocationOfUsers/views.py
@@ -1,5 +1,11 @@
+# usefull packages from django
 from django.shortcuts import render
 from django.views.generic import View
+from django.http import JsonResponse
+from django.utils import timezone
+
+# models 
+from AndroidRequests.models import DevicePositionInTime
 
 # Create your views here.
 
@@ -15,3 +21,25 @@ class MapHandler(View):
 		template = "googelMap.html"
 
 		return render(request, template, self.context)
+
+class GetMapPositions(View):
+	'''This class request to to the database the values of the actives users'''
+	
+	def __init__(self):
+		"""the contructor, context are the parameter given to the html template"""
+		self.context={}	
+
+	def get(self, request):
+		template = "googelMap.html"
+
+		now = timezone.now()
+		earlier = now - timezone.timedelta(minutes=10)
+
+		postions = DevicePositionInTime.objects.filter(timeStamp__range=(earlier,now))
+
+		print 'ask for poses'
+		response = []
+		for aPosition in postions:
+			response.append({'latitud': aPosition.longitud, 'longitud': aPosition.latitud})
+
+		return JsonResponse(response, safe=False)
