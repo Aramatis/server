cf3477b393d9d73fc223871732edb7c3decb06f1
server <you@example.com>
2015-10-20 16:56:28 -0300

    migrations

--- AndroidRequests/migrations/0001_initial.py
+++ AndroidRequests/migrations/0001_initial.py
@@ -11,6 +11,13 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='ActiveToken',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('timeStamp', models.DateTimeField()),
+            ],
+        ),
+        migrations.CreateModel(
             name='DevicePositionInTime',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
@@ -18,5 +25,33 @@ class Migration(migrations.Migration):
                 ('latitud', models.FloatField()),
                 ('timeStamp', models.DateTimeField()),
             ],
+        ),
+        migrations.CreateModel(
+            name='PoseInTrajectoryOfToken',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('longitud', models.FloatField()),
+                ('latitud', models.FloatField()),
+                ('timeStamp', models.DateTimeField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Token',
+            fields=[
+                ('tokenID', models.CharField(max_length=128, serialize=False, primary_key=True)),
+                ('busService', models.CharField(max_length=5)),
+                ('busRegistrationPlate', models.CharField(max_length=8)),
+                ('color', models.CharField(default=b'#00a0f0', max_length=7)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='poseintrajectoryoftoken',
+            name='token',
+            field=models.ForeignKey(to='AndroidRequests.Token'),
+        ),
+        migrations.AddField(
+            model_name='activetoken',
+            name='token',
+            field=models.ForeignKey(to='AndroidRequests.Token'),
         ),
     ]
