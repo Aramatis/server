8f7e071ea3d37d0ff0d8c66e696d724a7ffdea16
roberto <you@example.com>
2015-10-21 13:16:55 -0300

    fix in url

--- AndroidRequests/urls.py
+++ AndroidRequests/urls.py
@@ -3,8 +3,8 @@ from . import views
 from AndroidRequests.views import RequestToken, EndRoute, SendPoses
 urlpatterns = [
 	url(r'^nearbyBuses/(?P<pBusStop>\w+)$', views.nearbyBuses),
-    url(r'^userPosition/(?P<pLat>[-+]?[0-9]*\.?[0-9]*)/(?P<pLon>[-+]?[0-9]*\.?[0-9]*)$', views.userPosition),
-    url(r'^requestToken/(?P<pBusService>[0-9,a-z]*)/(?P<pRegistrationPlate>[0-9,A-Z]*)$', RequestToken.as_view()),
+    url(r'^userPosition/(?P<pLat>[\-+]?[0-9]*\.?[0-9]*)/(?P<pLon>[\-+]?[0-9]*\.?[0-9]*)$', views.userPosition),
+    url(r'^requestToken/(?P<pBusService>[0-9,\w]*)/(?P<pRegistrationPlate>[0-9,\w]*)$', RequestToken.as_view()),
     url(r'^endRoute/(?P<pToken>[0-9,a-f]{128})$', EndRoute.as_view()),
     url(r'^sendTrajectory/(?P<pToken>[0-9,a-f]{128})/(?P<pTrajectory>.*)$', SendPoses.as_view()),
 ]
