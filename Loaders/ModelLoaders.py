79b456d553080a8cf81c965ef3b86b569c3bdd8c
roberto <you@example.com>
2015-11-03 13:01:58 -0300

    the events where change

--- Loaders/ModelLoaders.py
+++ Loaders/ModelLoaders.py
@@ -157,60 +157,81 @@ class LoadEvents(object):
 	def loadEvents(self):
 		#Events for the bustop, the 5 digit of the id are in hexadecimal
 		# Events for bunching
-		Event.objects.create(id ='evn00000', name='Buses Juntos', description='Dos buses, o mas, del mismo servicio pasan juntos por la misma parada.',\
-			lifespam=5, category='bunching', origin='o', eventType='busStop')
+		Event.objects.create(id ='evn00000', name='2 juntos', description='Dos buses del mismo servicio pasan juntos por la misma parada.',\
+			lifespam=30, category='buses Juntos', origin='o', eventType='busStop')
+		Event.objects.create(id ='evn00001', name='3 o + juntos', description='Más de dos busesdel mismo servicio pasan juntos por la misma parada.',\
+			lifespam=30, category='buses Juntos', origin='o', eventType='busStop')
 		# Events for estado fisico
-		Event.objects.create(id ='evn00001', name='No hay techo', description='Al paradero le falta el techo completo o parte de él.',\
+		Event.objects.create(id ='evn00010', name='No hay techo', description='Al paradero le falta el techo completo o parte de él.',\
 			lifespam=43200, category='estado físico', origin='o', eventType='busStop')
-		Event.objects.create(id ='evn00002', name='No hay asientos', description='En el paradero no existe un lugar adecuado para sentarse',\
+		Event.objects.create(id ='evn00011', name='No hay asientos', description='En el paradero no existe un lugar adecuado para sentarse',\
 			lifespam=43200, category='estado físico', origin='o', eventType='busStop')
-		Event.objects.create(id ='evn00003', name='No hay iluminación', description='En el paradero no hay luces que lo iliminen.',\
+		Event.objects.create(id ='evn00012', name='No hay iluminación', description='En el paradero no hay luces que lo iliminen.',\
 			lifespam=43200, category='estado físico', origin='o', eventType='busStop')
-		Event.objects.create(id ='evn00004', name='No hay informacion de recorridos.', description='En el paradero no se especifica que servicios pasan por ahi.',\
+		Event.objects.create(id ='evn00013', name='No hay informacion de recorridos.', description='En el paradero no se especifica que servicios pasan por ahi.',\
 			lifespam=43200, category='estado físico', origin='o', eventType='busStop')
 		# Events for Robos o desordenes
-		Event.objects.create(id ='evn00005', name='Robos o desordenes', description='En el paradero se precencias robos o se han producido disturbios',\
+		Event.objects.create(id ='evn00020', name='Robos o desordenes', description='En el paradero se precencias robos o se han producido disturbios',\
 			lifespam=5, category='ambiente', origin='o', eventType='busStop')
-		Event.objects.create(id ='evn00006', name='Ebrio', description='Hay personas ebrais en el paradero',\
+		Event.objects.create(id ='evn00021', name='Ebrio', description='Hay personas ebrais en el paradero',\
 			lifespam=5, category='ambiente', origin='o', eventType='busStop')
-		Event.objects.create(id ='evn00007', name='Paradero lleno', description='El paradero esta repleto',\
+		Event.objects.create(id ='evn00022', name='Paradero lleno', description='El paradero esta repleto',\
 			lifespam=5, category='ambiente', origin='o', eventType='busStop')
 
 		#Events for the bus reported from the busStop
-		Event.objects.create(id ='evn00008', name='Bus en transito', description='El bus que estaba informado para pasar por el paradero paso en transito.',\
+		Event.objects.create(id ='evn00100', name='Bus en transito', description='El bus que estaba informado para pasar por el paradero paso en transito.',\
 			lifespam=3000, category='desde paradero', origin='o', eventType='bus')
-		Event.objects.create(id ='evn00009', name='Bus vacio no se detuvo', description='El bus estaba vacio o casi vacio de pasajeros y no paro en el paradero.',\
+		Event.objects.create(id ='evn00101', name='Bus vacio no se detuvo', description='El bus estaba vacio o casi vacio de pasajeros y no paro en el paradero.',\
 			lifespam=3000, category='desde paradero', origin='o', eventType='bus')
-		Event.objects.create(id ='evn0000a', name='Bus lleno no se detuvo', description='El bus estaba lleno de pasajeros y no paro en el paradero.',\
+		Event.objects.create(id ='evn00102', name='Bus lleno no se detuvo', description='El bus estaba lleno de pasajeros y no paro en el paradero.',\
 			lifespam=3000, category='desde parader0', origin='o', eventType='bus')
 
 		#Events for the bus
 		# Event for ambiente
-		Event.objects.create(id ='evn0000b', name='Bus lleno', description='El bus estaba lleno de pasajeros.',\
-			lifespam=3000, category='ambiente', origin='i', eventType='bus')
-		Event.objects.create(id ='evn0000c', name='Incidente a la vista', description='Se observa un taco o un accidente en la ruta.',\
-			lifespam=3000, category='ambiente', origin='i', eventType='bus')
-		Event.objects.create(id ='evn0000d', name='Conduccion brusca', description='El condictor del bus realisa maniobras inadecuadas al conducir.',\
-			lifespam=3000, category='ambiente', origin='i', eventType='bus')
-		Event.objects.create(id ='evn0000e', name='Hay asientos', description='El bus tiene asientos libres.',\
-			lifespam=3000, category='ambiente', origin='i', eventType='bus')
+		Event.objects.create(id ='evn00200', name='Imposible moverse', description='No se puede transitar por el bus, dada la cantidad de gente.',\
+			lifespam=5, category='ambiente', origin='i', eventType='bus')
+		Event.objects.create(id ='evn00201', name='Se puede caminar con dificultad', description='El bus esta lleno y es dificil circular por él.',\
+			lifespam=5, category='ambiente', origin='i', eventType='bus')
+		Event.objects.create(id ='evn00202', name='No hay asientos disponibles', description='No se puede serntar en ningun asiento.',\
+			lifespam=5, category='ambiente', origin='i', eventType='bus')
+		Event.objects.create(id ='evn00203', name='Hay asientos disponibles', description='El bus tiene asientos libres.',\
+			lifespam=5, category='ambiente', origin='i', eventType='bus')
 		# Event for indidentes en el bus
-		Event.objects.create(id ='evn0000f', name='Robos o desordenes', description='En el bus se presencio un robo, o hay disturbios en el.',\
-			lifespam=3000, category='incidentes en el bus', origin='i', eventType='bus')
-		Event.objects.create(id ='evn00010', name='Pasajero bebiendo y/o hebrio', description='El bus hay pasajero(s) bebiendo o hebrio(s).',\
-			lifespam=3000, category='incidentes en el bus', origin='i', eventType='bus')
-		Event.objects.create(id ='evn00011', name='Todo tranquilo', description='El bus esta bien.',\
-			lifespam=3000, category='incidentes en el bus', origin='i', eventType='bus')
+		Event.objects.create(id ='evn00210', name='Están Robando', description='En el bus se presencio un robo.',\
+			lifespam=5, category='incidentes en el bus', origin='i', eventType='bus')
+		Event.objects.create(id ='evn00211', name='Están Bebiendo', description='El bus hay pasajero(s) bebiendo.',\
+			lifespam=5, category='incidentes en el bus', origin='i', eventType='bus')
+		Event.objects.create(id ='evn00212', name='Grupo haciendo desorden', description='Hay gente haciendo desorden al interior del bus.',\
+			lifespam=5, category='incidentes en el bus', origin='i', eventType='bus')
+		Event.objects.create(id ='evn00213', name='Todo tranquilo', description='El bus esta bien.',\
+			lifespam=5, category='incidentes en el bus', origin='i', eventType='bus')
 		# Event for estado físico del bus
-		Event.objects.create(id ='evn00012', name='Puerta en mal estado', description='El bus tiene al menos una puerta en mal estado.',\
-			lifespam=3000, category='estado físico del bus', origin='i', eventType='bus')
-		Event.objects.create(id ='evn00013', name='Pasamanos en mal estado', description='El bus tiene al menos un pasamanos en mal estado.',\
-			lifespam=3000, category='estado físico del bus', origin='i', eventType='bus')
-		Event.objects.create(id ='evn00014', name='Ventana en mal estado', description='El bus tiene al menos una ventana en mal estado.',\
-			lifespam=3000, category='estado físico del bus', origin='i', eventType='bus')
-		Event.objects.create(id ='evn00015', name='Timbre en mal estado', description='El bus tiene al menos un timbre en mal estado.',\
-			lifespam=3000, category='estado físico del bus', origin='i', eventType='bus')
-		Event.objects.create(id ='evn00016', name='Sucio', description='El bus esta sucio.',\
-			lifespam=3000, category='estado físico del bus', origin='i', eventType='bus')
-		Event.objects.create(id ='evn00017', name='Bus en buen estado', description='El bus esta en buenas condiciones para funcionar.',\
-			lifespam=3000, category='estado físico del bus', origin='i', eventType='bus')
+		Event.objects.create(id ='evn00220', name='Puerta en mal estado', description='El bus tiene al menos una puerta en mal estado.',\
+			lifespam=3000, category='estado físico', origin='i', eventType='bus')
+		Event.objects.create(id ='evn00221', name='Pasamanos en mal estado', description='El bus tiene al menos un pasamanos en mal estado.',\
+			lifespam=3000, category='estado físico', origin='i', eventType='bus')
+		Event.objects.create(id ='evn00222', name='Ventana en mal estado', description='El bus tiene al menos una ventana en mal estado.',\
+			lifespam=3000, category='estado físico', origin='i', eventType='bus')
+		Event.objects.create(id ='evn00223', name='Timbre en mal estado', description='El bus tiene al menos un timbre en mal estado.',\
+			lifespam=3000, category='estado físico', origin='i', eventType='bus')
+		Event.objects.create(id ='evn00224', name='Sucio', description='El bus esta sucio.',\
+			lifespam=3000, category='estado físico', origin='i', eventType='bus')
+		Event.objects.create(id ='evn00225', name='Bus en buen estado', description='El bus esta en buenas condiciones para funcionar.',\
+			lifespam=3000, category='estado físico', origin='i', eventType='bus')
+		# Event for conductor
+		Event.objects.create(id ='evn00230', name='conducción brusca', description='El chofer raliza maniobras que hacer peligrar mi bien estar.',\
+			lifespam=30, category='conductor', origin='i', eventType='bus')
+		Event.objects.create(id ='evn00231', name='no para en parader', description='El bus no paro en el paradero, pese a que alguien pidio la parada desde el bus o el paradero.',\
+			lifespam=30, category='conductor', origin='i', eventType='bus')
+		Event.objects.create(id ='evn00232', name='habla por celular', description='El chofer hable por celular mientras maneja.',\
+			lifespam=5, category='conductor', origin='i', eventType='bus')
+		Event.objects.create(id ='evn00233', name='buen comportamiento', description='El bus va bien.',\
+			lifespam=5, category='conductor', origin='i', eventType='bus')
+		# Event for incidente en la via
+		Event.objects.create(id ='evn00240', name='taco', description='El bus esta en un taco.',\
+			lifespam=5, category='incidente en la via', origin='i', eventType='bus')
+		Event.objects.create(id ='evn00241', name='accidente', description='El bus paso cerca de un accidente.',\
+			lifespam=5, category='incidente en la via', origin='i', eventType='bus')
+		Event.objects.create(id ='evn00242', name='sin novedad', description='El bus va bien.',\
+			lifespam=5, category='incidente en la via', origin='i', eventType='bus')
+		
