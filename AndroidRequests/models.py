4a0c54fedf25fae89e801b3ac769584c47cac571
roberto <you@example.com>
2015-11-04 12:55:07 -0300

    fix syntax error

--- AndroidRequests/models.py
+++ AndroidRequests/models.py
@@ -68,8 +68,7 @@ class EventRegistration(models.Model):
 class EventForBusStop(EventRegistration):
 	'''This model stores the reported events for the busStop'''
 	busStop = models.ForeignKey('BusStop', verbose_name='The bustop')
-	aditionalInfo = models.CharField(max_length=140, default='none'):
-		pass)# particular informaction of the event	
+	aditionalInfo = models.CharField(max_length=140, default='none')
 
 
 class EventForBus(EventRegistration):
