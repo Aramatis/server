ec39b5c0d425f4ebb9b1788342547e5afab4335d
server <you@example.com>
2015-11-18 12:39:21 -0300

    cosas

--- parser.py
+++ parser.py
@@ -8,33 +8,37 @@ import django
 django.setup()
 from AndroidRequests.models import *
 
-exacta509 = open(os.path.join(os.getcwd(), 'InitialData', '509I-parada-loc-exacta.csv'), 'w')
-estima509 = open(os.path.join(os.getcwd(), 'InitialData', '509I-parada-loc-estima.csv'), 'w')
-exacta314 = open(os.path.join(os.getcwd(), 'InitialData', '314I-parada-loc-exacta.csv'), 'w')
-estima314 = open(os.path.join(os.getcwd(), 'InitialData', '314I-parada-loc-estima.csv'), 'w')
+exacta509 = open(os.path.join(os.getcwd(), 'InitialData', '506I-parada-loc-exacta.csv'), 'w')
+estima509 = open(os.path.join(os.getcwd(), 'InitialData', '506I-parada-loc-estima.csv'), 'w')
+#exacta314 = open(os.path.join(os.getcwd(), 'InitialData', '314I-parada-loc-exacta.csv'), 'w')
+#estima314 = open(os.path.join(os.getcwd(), 'InitialData', '314I-parada-loc-estima.csv'), 'w')
 exacta509.write("latitude; longitude;name\n")
 estima509.write("latitude; longitude;name\n")
-exacta314.write("latitude; longitude;name\n")
-estima314.write("latitude; longitude;name\n")
+#exacta314.write("latitude; longitude;name\n")
+#estima314.write("latitude; longitude;name\n")
 data = open(sys.argv[1], 'r')
 data509 = []
-data314 = []
+#data314 = []
 for line in data:
 	line.replace("\n", "")
 	datos = line.split(";")
 	datos[2] = datos[2].replace("\n", "").replace("\r", "")
-	busStop = BusStop.objects.get(code=datos[0])
-	if(datos[1]=="509I"):
+	try:
+		busStop = BusStop.objects.get(code=datos[0])
+	except:
+		print datos
+		continue
+	if(datos[1]=="506I"):
 		data509.append([datos[0], datos[1], datos[2]])
 		exacta509.write(str(busStop.latitud) + ";"+ str(busStop.longitud) + ";" + datos[0] + "\n")
-	else:
-		data314.append([datos[0], datos[1], datos[2]])
-		exacta314.write(str(busStop.latitud) + ";"+ str(busStop.longitud) + ";" + datos[0] + "\n")
-exacta314.close()
+#	else:
+#		data314.append([datos[0], datos[1], datos[2]])
+#		exacta314.write(str(busStop.latitud) + ";"+ str(busStop.longitud) + ";" + datos[0] + "\n")
+#exacta314.close()
 exacta509.close()
 
 for data in data509:
-	serviceCode = "509I"
+	serviceCode = "506I"
 	ssd = round(int(data[2]), -1)
 	try:
 		location = ServiceLocation.objects.filter(service = serviceCode, distance = ssd)[0]
@@ -42,6 +46,7 @@ for data in data509:
 	except:
 		print data
 estima509.close()
+'''
 for data in data314:
 	serviceCode = "314I"
 	ssd = round(int(data[2]), -1)
@@ -52,3 +57,4 @@ for data in data314:
 	except:
 		print data
 estima314.close()
+'''
