5694c32489f5363fdfbc62eb46d8fd854106e61a
roberto <you@example.com>
2015-11-19 16:03:59 -0300

    bug prints

--- AndroidRequests/models.py
+++ AndroidRequests/models.py
@@ -188,7 +188,7 @@ class Bus(models.Model):
 	def __estimatedPosition(self, busstop, distance):
 		'''Given a distace from the bus to the busstop, this method returns the global position of
 		the machine.'''
-		print 'asss',busstop, distance,self.service
+		print 'asss',busstop, len(busstop), distance,self.service
 		try:
 			serviceCode = ServicesByBusStop.objects.get(busStop = busstop, service = self.service).code
 			print 'passThis'
