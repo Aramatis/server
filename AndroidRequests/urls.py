57f8dab84f11e05ce51dde496557941bba8e6bb1
Felipe Hernández <cephei.1313@gmail.com>
2016-05-03 16:22:56 -0300

    each url receives a userId in the first parameter

--- AndroidRequests/urls.py
+++ AndroidRequests/urls.py
@@ -13,18 +13,20 @@ from AndroidRequests.allviews.BusStopsByService import *
 from AndroidRequests.allviews.RegisterReport import *
 
 urlpatterns = [
-	url(r'^nearbyBuses/(?P<pBusStop>\w+)$', views.nearbyBuses),
-    url(r'^registerReport$', RegisterReport.as_view()),
-    url(r'^userPosition/(?P<pLat>[\-+]?[0-9]*\.?[0-9]*)/(?P<pLon>[\-+]?[0-9]*\.?[0-9]*)$', views.userPosition),
-    url(r'^requestToken/(?P<pBusService>[0-9,\w]*)/(?P<pRegistrationPlate>[0-9,\w]*)$', RequestToken.as_view()),
-    url(r'^endRoute/(?P<pToken>[0-9,a-f]{128})$', EndRoute.as_view()),
-    url(r'^sendTrajectory/(?P<pToken>[0-9,a-f]{128})/(?P<pTrajectory>.*)$', SendPoses.as_view()),
-    url(r'^reportEventBus/(?P<pBusService>[\w,0-9]*)/(?P<pBusPlate>[\w,0-9]*)/(?P<pEventID>.*)/(?P<pLatitud>[\-+]?[0-9]*\.?[0-9]*)/(?P<pLongitud>[\-+]?[0-9]*\.?[0-9]*)/(?P<pConfirmDecline>.*)$', RegisterEventBus.as_view()),
-    url(r'^reportEventBus/(?P<pBusService>[\w,0-9]*)/(?P<pBusPlate>[\w,0-9]*)/(?P<pEventID>.*)/(?P<pConfirmDecline>.*)$', RegisterEventBus.as_view()),
-    url(r'^reportEventBusStop/(?P<pBusStopCode>[\w,0-9]*)/(?P<pEventID>.*)/(?P<pLatitud>[\-+]?[0-9]*\.?[0-9]*)/(?P<pLongitud>[\-+]?[0-9]*\.?[0-9]*)/(?P<pConfirmDecline>.*)$', RegisterEventBusStop.as_view()),
-    url(r'^reportEventBusStop/(?P<pBusStopCode>[\w,0-9]*)/(?P<pEventID>.*)/(?P<pConfirmDecline>.*)$', RegisterEventBusStop.as_view()),
-    url(r'^requestEventsForBus/(?P<pRegistrationPlate>[\w,0-9]*)/(?P<pBusService>[\w,0-9]*)$', EventsByBus.as_view()),
-    url(r'^requestEventsToNotified/(?P<pWhich>[\w,0-9]*)$', RequestEventsToNotified.as_view()),
-    url(r'^requestEventsForBusStop/(?P<pBusStopCode>[\w,0-9]*)$', EventsByBusStop.as_view()),
-    url(r'^requestBusStopsForService/(?P<pBusService>[\w,0-9]*)$', BusStopsByService.as_view()),
+	url(r'^nearbyBuses/(?P<pUserId>[0-9a-z-]+)/(?P<pBusStop>\w+)$', views.nearbyBuses),
+    url(r'^registerReport/(?P<pUserId>[0-9a-z-]+)$', RegisterReport.as_view()),
+    url(r'^userPosition/(?P<pUserId>[0-9a-z-]+)/(?P<pLat>[\-+]?[0-9]*\.?[0-9]*)/(?P<pLon>[\-+]?[0-9]*\.?[0-9]*)$', views.userPosition),
+    url(r'^requestToken/(?P<pUserId>[0-9a-z-]+)/(?P<pBusService>[0-9,\w]*)/(?P<pRegistrationPlate>[0-9,\w]*)$', RequestToken.as_view()),
+    url(r'^endRoute/(?P<pUserId>[0-9a-z-]+)/(?P<pToken>[0-9,a-f]{128})$', EndRoute.as_view()),
+    url(r'^sendTrajectory/(?P<pUserId>[0-9a-z-]+)/(?P<pToken>[0-9,a-f]{128})/(?P<pTrajectory>.*)$', SendPoses.as_view()),
+    # reportEventBus with location
+    url(r'^reportEventBus/(?P<pUserId>[0-9a-z-]+)/(?P<pBusService>[\w,0-9]*)/(?P<pBusPlate>[\w,0-9]*)/(?P<pEventID>.*)/(?P<pLatitud>[\-+]?[0-9]*\.?[0-9]*)/(?P<pLongitud>[\-+]?[0-9]*\.?[0-9]*)/(?P<pConfirmDecline>.*)$', RegisterEventBus.as_view()),
+    # reportEventBus without location
+    url(r'^reportEventBus/(?P<pUserId>[0-9a-z-]+)/(?P<pBusService>[\w,0-9]*)/(?P<pBusPlate>[\w,0-9]*)/(?P<pEventID>.*)/(?P<pConfirmDecline>.*)$', RegisterEventBus.as_view()),
+    url(r'^reportEventBusStop/(?P<pUserId>[0-9a-z-]+)/(?P<pBusStopCode>[\w,0-9]*)/(?P<pEventID>.*)/(?P<pLatitud>[\-+]?[0-9]*\.?[0-9]*)/(?P<pLongitud>[\-+]?[0-9]*\.?[0-9]*)/(?P<pConfirmDecline>.*)$', RegisterEventBusStop.as_view()),
+    url(r'^reportEventBusStop/(?P<pUserId>[0-9a-z-]+)/(?P<pBusStopCode>[\w,0-9]*)/(?P<pEventID>.*)/(?P<pConfirmDecline>.*)$', RegisterEventBusStop.as_view()),
+    url(r'^requestEventsForBus/(?P<pUserId>[0-9a-z-]+)/(?P<pRegistrationPlate>[\w,0-9]*)/(?P<pBusService>[\w,0-9]*)$', EventsByBus.as_view()),
+    url(r'^requestEventsToNotified/(?P<pUserId>[0-9a-z-]+)/(?P<pWhich>[\w,0-9]*)$', RequestEventsToNotified.as_view()),
+    url(r'^requestEventsForBusStop/(?P<pUserId>[0-9a-z-]+)/(?P<pBusStopCode>[\w,0-9]*)$', EventsByBusStop.as_view()),
+    url(r'^requestBusStopsForService/(?P<pUserId>[0-9a-z-]+)/(?P<pBusService>[\w,0-9]*)$', BusStopsByService.as_view()),
 ]
