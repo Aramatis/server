c6c32535d14c968b80e58569407a7ab147a7af4e
Felipe Hernández <cephei.1313@gmail.com>
2016-06-23 10:20:39 -0400

    registration plate has length between 6 and 8

--- AndroidRequests/urls.py
+++ AndroidRequests/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
 	url(r'^nearbyBuses/(?P<pUserId>[0-9a-z-]+)/(?P<pBusStop>\w+)$', views.nearbyBuses),
     url(r'^registerReport$', RegisterReport.as_view()),
     url(r'^userPosition/(?P<pUserId>[0-9a-z-]+)/(?P<pLat>[\-+]?[0-9]*\.?[0-9]*)/(?P<pLon>[\-+]?[0-9]*\.?[0-9]*)$', views.userPosition),
-    url(r'^requestToken/(?P<pUserId>[0-9a-z-]+)/(?P<pBusService>[0-9,\w]*)/(?P<pRegistrationPlate>[0-9,\w]*)$', RequestToken.as_view()),
+    url(r'^requestToken/(?P<pUserId>[0-9a-z-]+)/(?P<pBusService>[0-9,\w]*)/(?P<pRegistrationPlate>[0-9,\w]{6,8})$', RequestToken.as_view()),
     url(r'^endRoute/(?P<pToken>[0-9,a-f]{128})$', EndRoute.as_view()),
     url(r'^sendTrajectory/(?P<pToken>[0-9,a-f]{128})/(?P<pTrajectory>.*)$', SendPoses.as_view()),
     # reportEventBus with location
@@ -28,7 +28,7 @@ urlpatterns = [
     # List of events that depend of parameter pWhich={stopstop,stopbus, busbus}
     url(r'^requestEventsToNotified/(?P<pWhich>[\w,0-9]*)$', RequestEventsToNotified.as_view()),
     # List of bus events
-    url(r'^requestEventsForBus/(?P<pRegistrationPlate>[\w,0-9]*)/(?P<pBusService>[\w,0-9]*)$', EventsByBus.as_view()),
+    url(r'^requestEventsForBus/(?P<pRegistrationPlate>[\w,0-9]{6,8})/(?P<pBusService>[\w,0-9]*)$', EventsByBus.as_view()),
     # List of bus stop events
     url(r'^requestEventsForBusStop/(?P<pBusStopCode>[\w,0-9]*)$', EventsByBusStop.as_view()),
     # List of bus stop of a service
