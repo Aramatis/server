fb1d0a75c30d7a21d1dbbe6cff8039328f7de719
Felipe Hernández <cephei.1313@gmail.com>
2016-07-08 16:39:12 -0400

    retrieve variant IDA and REGRESO from database.

--- AndroidRequests/allviews/ServiceRoute.py
+++ AndroidRequests/allviews/ServiceRoute.py
@@ -9,34 +9,53 @@ class ServiceRoute(View):
     def __init__(self):
         self.context={}
 
-    def get(self, request, pBusService, pLat1, pLong1, pLat2, pLong2):
+    def get(self, request, pBusService, pLat1, pLon1, pLat2, pLon2):
         """it receive the bus Service to get points that creates service route """
         response = {}
-        response['service'] = pBusService
+        response['statusCode'] = "200"
+        response['statusMessage'] = "ok"
         response['route'] = []
 
-        # ask for the bus stops for this service
-        serviceCode = pBusService + self.detectRouteDirection(pBusService, pLat1, pLong1, pLat2, pLong2)
-        route = self.getRouteForService(serviceCode)
+        # ask for the route for this service
+        route = self.getRoutesForService(pBusService)
+
+        #serviceCode = pBusService + self.detectRouteDirection(pBusService, pLat1, pLon1, pLat2, pLon2)
+
+        response['service'] = pBusService
+
+        if len(route) == 0:
+            response['statusCode'] = "300"
+            response['statusMessage'] = "Service does not have route in the database."
 
         response['route'] = route
 
         return JsonResponse(response, safe=False)
 
-    def detectRouteDirection(self, pBusService, pLat1, pLong1, pLat2, pLong2):
+    def detectRouteDirection(self, pBusService, pLat1, pLon1, pLat2, pLon2):
         """ detect route direction based on two points  """
         #TODO: @fhernandez implement a criteria
         return 'I'
 
-    def getRouteForService(self,pServiceCode):
+    def getRoutesForService(self, pServiceCode):
         """ return the route defined for a service. """
         route = []
 
-        for point in Route.objects.filter(serviceCode=pServiceCode).order_by('sequence'):
-            data = {}
-            data['latitude'] = point.latitud
-            data['longitude'] = point.longitud
-            data['sequence'] = point.sequence
-            route.append(data)
+        service = pServiceCode + '[a-zA-Z]*' # all variants
+        service = pServiceCode + '[I,R]$' # variant: IDA and REGRESO
+
+        # get all services associated to number
+        for service in Route.objects.values('serviceCode').filter(serviceCode__regex=service).distinct():
+            variantCode = service['serviceCode']
+            variant = {}
+            variant['variant'] = variantCode
+            variant['route'] = []
+            for point in Route.objects.filter(serviceCode=variantCode).order_by('sequence'):
+                #print "{} {}".format(point.serviceCode, point.sequence)
+                data = {}
+                data['latitude'] = point.latitud
+                data['longitude'] = point.longitud
+                data['sequence'] = point.sequence
+                variant['route'].append(data)
+            route.append(variant)
 
         return route
