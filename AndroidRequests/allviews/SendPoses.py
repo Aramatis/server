aa783d9bab20165ec7b5707e00a9b30f501787d3
Felipe Hernández <cephei.1313@gmail.com>
2016-07-20 14:11:47 -0400

    ask if the parameters were sending by POST

--- AndroidRequests/allviews/SendPoses.py
+++ AndroidRequests/allviews/SendPoses.py
@@ -17,30 +17,31 @@ class SendPoses(View):
     def get(self, request):
         response = {}
 
-        pToken = request.POST.get('pToken', '')
-        pTrajectory = request.POST.get('pTrajectory', '')
+        if request.method == 'POST':
+            pToken = request.POST.get('pToken', '')
+            pTrajectory = request.POST.get('pTrajectory', '')
 
-        if ActiveToken.objects.filter(token=pToken).exists():
-            trajectory = json.loads(pTrajectory)
-            trajectory = trajectory['poses']
+            if ActiveToken.objects.filter(token=pToken).exists():
+                trajectory = json.loads(pTrajectory)
+                trajectory = trajectory['poses']
 
-            #update the token time stamp, for maintanence purpuses
-            aToken = ActiveToken.objects.get(token=pToken)
-            aToken.timeStamp = timezone.now()
-            aToken.save()
+                #update the token time stamp, for maintanence purpuses
+                aToken = ActiveToken.objects.get(token=pToken)
+                aToken.timeStamp = timezone.now()
+                aToken.save()
 
-            theToken = Token.objects.get(token=pToken)
+                theToken = Token.objects.get(token=pToken)
 
-            for pose in trajectory:
-                # set awareness to time stamp, to the server UTC
-                aTimeStamp = dateparse.parse_datetime(pose['timeStamp'])
-                aTimeStamp = timezone.make_aware(aTimeStamp)
+                for pose in trajectory:
+                    # set awareness to time stamp, to the server UTC
+                    aTimeStamp = dateparse.parse_datetime(pose['timeStamp'])
+                    aTimeStamp = timezone.make_aware(aTimeStamp)
 
-                PoseInTrajectoryOfToken.objects.create(longitud=pose['longitud'],latitud=pose['latitud'],\
-                        timeStamp=aTimeStamp, inVehicleOrNot=pose["inVehicleOrNot"],token=theToken)
+                    PoseInTrajectoryOfToken.objects.create(longitud=pose['longitud'],latitud=pose['latitud'],\
+                            timeStamp=aTimeStamp, inVehicleOrNot=pose["inVehicleOrNot"],token=theToken)
 
-            response['response'] = 'Poses were register.'
-        else:#if the token was not found alert
-            response['response'] = 'Token doesn\'t exist.'
+                response['response'] = 'Poses were register.'
+            else:#if the token was not found alert
+                response['response'] = 'Token doesn\'t exist.'
 
         return JsonResponse(response, safe=False)
