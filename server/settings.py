8bc61ef89ccbf25ba6c15321ba76d9caafda54d8
Felipe Hernández <cephei.1313@gmail.com>
2016-08-26 08:51:53 -0300

    fixed method name used by cronTask

--- server/settings.py
+++ server/settings.py
@@ -181,7 +181,7 @@ MEDIA_IMAGE = os.path.join(MEDIA_ROOT, "reported_images/")
 
 CRONJOBS = [
     #the job is executed every day at 24
-    ('0 0 * * *', 'AndroidRequests.cronTasks.clearnEventsThatHaveBeenDecline'),
+    ('0 0 * * *', 'AndroidRequests.cronTasks.clearEventsThatHaveBeenDecline'),
     ('* */1 * * *', 'AndroidRequests.cronTasks.cleanActiveTokenTable')
 ]
 
