dbaa22d165092f30151f1a6b24518ba97b82bf40
sebastian Ormeno <seba_orme@hotmail.com>
2016-11-04 13:30:48 -0300

    modify function to resive multiples parameters

--- VisualizationBackupApp/jobs.py
+++ VisualizationBackupApp/jobs.py
@@ -3,14 +3,24 @@ import os
 
 
 
-def _run_script(filename):
+def _run_script(filename, *args):
 	app_path = os.path.dirname(os.path.realpath(__file__))
 	command = "bash " + app_path  + "/scripts/" + filename
 	command = command + " " + app_path
+	for arg in list(args):
+		command = command +" "+ arg
 	subprocess.call(command, shell=True)
+	
 
 def complete_dump():
-	_run_script("dump.sh")
+	VIZ_APP_FLDR = os.path.dirname(os.path.realpath(__file__))
+	REMOTE_USER = "transapp"
+	REMOTE_HOST = "104.236.183.105"
+	REMOTE_BKP_FLDR = "ftp_incoming"
+	PRIVATE_KEY = "/home/server/.ssh/id_rsa"
+	TMP_BKP_FLDR = "/tmp/backup_viz"
+	_run_script("dump.sh", VIZ_APP_FLDR, REMOTE_USER, REMOTE_HOST, REMOTE_BKP_FLDR, PRIVATE_KEY, TMP_BKP_FLDR)
+
 
 def partial_dump():
 	_run_script("partial_dump.sh")
