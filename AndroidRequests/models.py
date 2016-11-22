a0c9f23593bf361938af248254b23b90ad204292
Felipe Hernández <cephei.1313@gmail.com>
2016-11-22 17:01:07 -0300

    fixed json serializer for UUID fields

--- AndroidRequests/models.py
+++ AndroidRequests/models.py
@@ -5,6 +5,19 @@ from random import uniform
 
 import logging
 
+'''
+Dealing with no UUID serialization support in json
+'''
+from json import JSONEncoder
+from uuid import UUID
+
+JSONEncoder_olddefault = JSONEncoder.default
+def JSONEncoder_newdefault(self, o):
+    if isinstance(o, UUID): 
+        return str(o)
+    return JSONEncoder_olddefault(self, o)
+JSONEncoder.default = JSONEncoder_newdefault
+
 # Create your models here.
 # Remembre to add new models to admin.py
 
