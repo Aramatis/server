3b9cdf6370125b10768beb871e3602d9ad0930dd
Felipe Hernández <cephei.1313@gmail.com>
2016-12-29 17:35:39 -0300

    added variant of reportEventBusStop that accepts aditional info. Now it is service code

--- AndroidRequests/urls.py
+++ AndroidRequests/urls.py
@@ -28,31 +28,29 @@ urlpatterns = [
     url(
         r'^requestToken/(?P<pUserId>[0-9a-z-]+)/(?P<pBusService>[0-9,\w]*)/(?P<pRegistrationPlate>[0-9,\w,-]{6,8})$',
         RequestToken.as_view()),
-    url(
-        r'^requestToken/v2/(?P<pUserId>[0-9a-z-]+)/(?P<pBusService>[0-9,\w]*)/(?P<pUUID>[0-9a-z-]+)$',
-        RequestTokenV2.as_view()),
     url(r'^endRoute/(?P<pToken>[0-9,a-f]{128})$', EndRoute.as_view()),
     url(r'^sendTrajectory$', SendPoses.as_view()),
     # reportEventBus with location
     url(
         r'^reportEventBus/(?P<pUserId>[0-9a-z-]+)/(?P<pBusService>[\w,0-9]*)/(?P<pBusPlate>[\w,0-9,-]*)/(?P<pEventID>.*)/(?P<pLatitud>[\-+]?[0-9]*\.?[0-9]*)/(?P<pLongitud>[\-+]?[0-9]*\.?[0-9]*)/(?P<pConfirmDecline>.*)$',
         RegisterEventBus.as_view()),
-    url(
-        r'^reportEventBus/v2/(?P<pUserId>[0-9a-z-]+)/(?P<pUuid>[0-9a-z-]+)/(?P<pBusService>[\w,0-9]*)/(?P<pEventID>.*)/(?P<pLatitud>[\-+]?[0-9]*\.?[0-9]*)/(?P<pLongitud>[\-+]?[0-9]*\.?[0-9]*)/(?P<pConfirmDecline>.*)$',
-        RegisterEventBusV2.as_view()),
     # reportEventBus without location
     url(
         r'^reportEventBus/(?P<pUserId>[0-9a-z-]+)/(?P<pBusService>[\w,0-9]*)/(?P<pBusPlate>[\w,0-9,-]*)/(?P<pEventID>.*)/(?P<pConfirmDecline>.*)$',
         RegisterEventBus.as_view()),
-    url(
-        r'^reportEventBus/v2/(?P<pUserId>[0-9a-z-]+)/(?P<pUuid>[0-9a-z-]+)/(?P<pBusService>[\w,0-9]*)/(?P<pEventID>.*)/(?P<pConfirmDecline>.*)$',
-        RegisterEventBusV2.as_view()),
     url(
         r'^reportEventBusStop/(?P<pUserId>[0-9a-z-]+)/(?P<pBusStopCode>[\w,0-9]*)/(?P<pEventID>.*)/(?P<pLatitud>[\-+]?[0-9]*\.?[0-9]*)/(?P<pLongitud>[\-+]?[0-9]*\.?[0-9]*)/(?P<pConfirmDecline>.*)$',
         RegisterEventBusStop.as_view()),
     url(
         r'^reportEventBusStop/(?P<pUserId>[0-9a-z-]+)/(?P<pBusStopCode>[\w,0-9]*)/(?P<pEventID>.*)/(?P<pConfirmDecline>.*)$',
         RegisterEventBusStop.as_view()),
+    url(
+        r'^reportEventBusStop/(?P<pUserId>[0-9a-z-]+)/(?P<pBusStopCode>[\w,0-9]*)/(?P<pService>[0-9,\w]*)/(?P<pEventID>.*)/(?P<pLatitud>[\-+]?[0-9]*\.?[0-9]*)/(?P<pLongitud>[\-+]?[0-9]*\.?[0-9]*)/(?P<pConfirmDecline>.*)$',
+        RegisterEventBusStop.as_view()),
+    url(
+        r'^reportEventBusStop/(?P<pUserId>[0-9a-z-]+)/(?P<pBusStopCode>[\w,0-9]*)/(?P<pService>[0-9,\w]*)/(?P<pEventID>.*)/(?P<pConfirmDecline>.*)$',
+        RegisterEventBusStop.as_view()),
+ 
     # List of events that depend of parameter pWhich={stopstop,stopbus, busbus}
     url(r'^requestEventsToNotified/(?P<pWhich>[\w,0-9]*)$',
         RequestEventsToNotified.as_view()),
@@ -60,8 +58,6 @@ urlpatterns = [
     url(
         r'^requestEventsForBus/(?P<pRegistrationPlate>[\w,0-9,-]{6,8})/(?P<pBusService>[\w,0-9]*)$',
         EventsByBus.as_view()),
-    url(r'^requestEventsForBus/v2/(?P<pUuid>[0-9a-z-]+)$',
-        EventsByBusV2.as_view()),
     # List of bus stop events
     url(
         r'^requestEventsForBusStop/(?P<pBusStopCode>[\w,0-9]*)$',
@@ -77,4 +73,18 @@ urlpatterns = [
     url(r'^setDirection$', SetDirection.as_view()),
     url(r'^getUUID/(?P<pLicensePlate>[\w,0-9,-]{6,8})$',
         RequestUUID.as_view()),
+    # =====================================================
+    # VERSION 2
+    # =====================================================
+    url(
+        r'^requestToken/v2/(?P<pUserId>[0-9a-z-]+)/(?P<pBusService>[0-9,\w]*)/(?P<pUUID>[0-9a-z-]+)$',
+        RequestTokenV2.as_view()),
+    url(
+        r'^reportEventBus/v2/(?P<pUserId>[0-9a-z-]+)/(?P<pUuid>[0-9a-z-]+)/(?P<pBusService>[\w,0-9]*)/(?P<pEventID>.*)/(?P<pLatitud>[\-+]?[0-9]*\.?[0-9]*)/(?P<pLongitud>[\-+]?[0-9]*\.?[0-9]*)/(?P<pConfirmDecline>.*)$',
+        RegisterEventBusV2.as_view()),
+    url(
+        r'^reportEventBus/v2/(?P<pUserId>[0-9a-z-]+)/(?P<pUuid>[0-9a-z-]+)/(?P<pBusService>[\w,0-9]*)/(?P<pEventID>.*)/(?P<pConfirmDecline>.*)$',
+        RegisterEventBusV2.as_view()),
+    url(r'^requestEventsForBus/v2/(?P<pUuid>[0-9a-z-]+)$',
+        EventsByBusV2.as_view()),
 ]
