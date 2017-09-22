c4bc58fbaaf662dbd05eac8a4a5c3ea1abb8280f
Felipe Hernández <cephei.1313@gmail.com>
2017-09-22 16:14:47 -0300

    test of authority predictor

--- PredictorDTPM/tests.py
+++ PredictorDTPM/tests.py
@@ -2,7 +2,7 @@
 from django.test import TestCase, RequestFactory
 from django.conf import settings
 
-from mock import MagicMock
+from mock import Mock
 
 from PredictorDTPM.webService.WebService import WebService
 
@@ -13,20 +13,145 @@ class ParserTest(TestCase):
     def setUp(self):
         """  """
         self.factory = RequestFactory()
-        stopCode = 'PA433'
-        url = 'dtpm/busStopInfo/{}/{}'.format(settings.SECRET_KEY, stopCode)
-        request = self.factory.get(url)
+        self.stopCode = 'PA433'
+        url = 'dtpm/busStopInfo/{}/{}'.format(settings.SECRET_KEY, self.stopCode)
+        self.request = self.factory.get(url)
 
-        # it is necessary to don´t initialize instance
-        WebService.clientInstance = []
-        self.webService = WebService(request)
-        data = self.webService.askForServices(stopCode)
+    def get_mock_client(self, sud_answer):
+        """  """
+        # create mock of WebServiceClient instance
+        client = Mock(spec=WebService.WebServiceClient)
 
-        self.auth_answer = {
+        sudClient = Mock()
+        sudClient.attach_mock(Mock(), "service")
+        sudClient.service.configure_mock(**{"predictorParaderoServicio.return_value": sud_answer})
 
+        client.client = sudClient
+        client.prefix = "t"
+        client.clientCode = "e"
+        client.answerCode = "s"
+        client.resCode = [{"code": "00"}]
+        client.transactionId = 1
+
+        return client
+
+    def test_code00AndCode01(self):
+        """ test situation when authority response routes with code 00 and 01  """
+        sud_answer = {
+            "fechaprediccion": "2017-09-22",
+            "horaprediccion": "15:32",
+            "nomett": "SEGUNDA TRANSVERSAL ESQ. / 3 NORTE",
+            "paradero": "PI576",
+            "respuestaParadero": None,
+            "servicios": [
+                [
+                    {
+                        "codigorespuesta": "00",
+                        "distanciabus1": "2155",
+                        "distanciabus2": "3095",
+                        "horaprediccionbus1": "Entre 07 Y 11 min. ",
+                        "horaprediccionbus2": "Entre 12 Y 18 min. ",
+                        "ppubus1": "FLXJ-24",
+                        "ppubus2": "FLXG-74",
+                        "respuestaServicio": None,
+                        "servicio": "I10",
+                    },
+                    {
+                        "codigorespuesta": "01",
+                        "distanciabus1": "1134",
+                        "distanciabus2": None,
+                        "horaprediccionbus1": "Menos de 5 min. ",
+                        "horaprediccionbus2": None,
+                        "ppubus1": "ZN-6513",
+                        "ppubus2": None,
+                        "respuestaServicio": None,
+                        "servicio": "423",
+                    }
+                ]
+            ],
+            "urlLinkPublicidad": None,
+            "urlPublicidad": "http://mkt.smsbus.cl/img/Cat11.jpg"
         }
 
-    def test_er(self):
-        """  """
+        sud_client = self.get_mock_client(sud_answer)
+        WebService.clientInstance = sud_client
+        webService = WebService(self.request)
 
-        pass
+        webService.askForServices(self.stopCode)
+
+    def test_code11(self):
+        """ test situation when authority response routes with code 11 """
+        sud_answer = {
+            "fechaprediccion": "2017-09-22",
+            "horaprediccion": "15:32",
+            "nomett": "SEGUNDA TRANSVERSAL ESQ. / 3 NORTE",
+            "paradero": "PI576",
+            "respuestaParadero": None,
+            "servicios": [
+                [
+                    {
+                        "codigorespuesta": "11",
+                        "distanciabus1": None,
+                        "distanciabus2": None,
+                        "horaprediccionbus1": None,
+                        "horaprediccionbus2": None,
+                        "ppubus1": None,
+                        "ppubus2": None,
+                        "respuestaServicio": "Fuera de horario de operacion para este paradero",
+                        "servicio": "I10N",
+                    },
+                    {
+                        "codigorespuesta": "10",
+                        "distanciabus1": None,
+                        "distanciabus2": None,
+                        "horaprediccionbus1": None,
+                        "horaprediccionbus2": None,
+                        "ppubus1": None,
+                        "ppubus2": None,
+                        "respuestaServicio": "No hay buses que se dirijan al paradero.",
+                        "servicio": "I13",
+                    }
+                ]
+            ],
+            "urlLinkPublicidad": None,
+            "urlPublicidad": "http://mkt.smsbus.cl/img/Cat11.jpg"
+        }
+
+        sud_client = self.get_mock_client(sud_answer)
+        WebService.clientInstance = sud_client
+        webService = WebService(self.request)
+
+        webService.askForServices(self.stopCode)
+
+    def test_code23(self):
+        """ test situation when authority response routes with code 11 """
+        sud_answer = {
+            "fechaprediccion": "2017-09-21",
+            "horaprediccion": "23:56",
+            "nomett": None,
+            "paradero": "PH607",
+            "respuestaParadero": "Paradero invalido.",
+            "servicios": [
+                [
+                    {
+                        "codigorespuesta": "23",
+                        "distanciabus1": None,
+                        "distanciabus2": None,
+                        "horaprediccionbus1": None,
+                        "horaprediccionbus2": None,
+                        "ppubus1": None,
+                        "ppubus2": None,
+                        "respuestaServicio": "Paradero invalido.",
+                        "servicio": None,
+                    }
+                ]
+            ],
+            "urlLinkPublicidad": None,
+            "urlPublicidad": "http://mkt.smsbus.cl/img/Cat11.jpg"
+        }
+
+        sud_client = self.get_mock_client(sud_answer)
+        WebService.clientInstance = sud_client
+        webService = WebService(self.request)
+
+        webService.askForServices(self.stopCode)
--- PredictorDTPM/webService/WebService.py
+++ PredictorDTPM/webService/WebService.py
@@ -11,29 +11,29 @@ from PredictorDTPM.models import Log
 class WebService:
     """ Communicate with TranSantiago predictor service """
 
-    class __WebServiceClient:
+    class WebServiceClient:
         """ Singleton for suds client """
 
         def __init__(self):
 
             with open(os.path.join(os.path.dirname(__file__), 
-                    '../../server/keys/DTPMConnectionParams.json')) as data_file:
+                    "../../server/keys/DTPMConnectionParams.json")) as data_file:
                 info = json.load(data_file)
 
             # WSDL url. it was gotten throught Wireless-IQ
-            self.client = Client(info['wsdl'])
+            self.client = Client(info["wsdl"])
             # prefix for webTransId
-            self.prefix = info['prefix']
+            self.prefix = info["prefix"]
             # client code to identify who is querying data
-            self.clientCode = info['clientCode']
+            self.clientCode = info["clientCode"]
             # answer code returned for each service
-            self.answerCode = info['answerCode']
+            self.answerCode = info["answerCode"]
             # resolution code: code, pixel size image, device resolution
             # used for makerting purposes, ignored by us
-            self.resCode = info['resolutionCode']
+            self.resCode = info["resolutionCode"]
             # transactionId
             try:
-                webTransId = Log.objects.order_by('-webTransId').first().webTransId
+                webTransId = Log.objects.order_by("-webTransId").first().webTransId
                 webTransId = int(webTransId.replace(self.prefix, "")) + 1
             except:
                 webTransId = 2600
@@ -48,13 +48,13 @@ class WebService:
         """ constructor """
         self.__ipFinalUser = self.__getUserIP(request)
         if WebService.clientInstance is None:
-            WebService.clientInstance = WebService.__WebServiceClient()
+            WebService.clientInstance = WebService.WebServiceClient()
 
     def askForServices(self, busStop):
         """ ask for services to TranSantiago """
         client = WebService.clientInstance.client
         clientCode = WebService.clientInstance.clientCode
-        resCode = WebService.clientInstance.resCode[0]['code']
+        resCode = WebService.clientInstance.resCode[0]["code"]
         ipFinalUser = self.__ipFinalUser
         webTransId = WebService.clientInstance.prefix + \
                 self.__completeId(WebService.clientInstance.transactionId)
@@ -64,26 +64,26 @@ class WebService:
         #        .format(clientCode, resCode, ipFinalUser, webTransId)
 
         result = client.service.predictorParaderoServicio(
-                    paradero = busStop,
-                    cliente = clientCode,
-                    resolucion = resCode,
-                    ipUsuarioFinal = ipFinalUser,
-                    webTransId = webTransId
+                    paradero=busStop,
+                    cliente=clientCode,
+                    resolucion=resCode,
+                    ipUsuarioFinal=ipFinalUser,
+                    webTransId=webTransId
                     )
 
         data = self.__parserDTPMData(result)
 
         # add web trans id to log in database
-        data['webTransId'] = webTransId
+        data["webTransId"] = webTransId
 
         return data
 
     def __getUserIP(self, request):
-        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
         if x_forwarded_for:
-            ip = x_forwarded_for.split(',')[0]
+            ip = x_forwarded_for.split(",")[0]
         else:
-            ip = request.META.get('REMOTE_ADDR')
+            ip = request.META.get("REMOTE_ADDR")
         return ip
 
     def __completeId(self, number):
@@ -94,19 +94,19 @@ class WebService:
         """ separate buses. Each one in an array """
 
         response = {
-            'fechaConsulta': dtpmInfo['fechaprediccion'],
-            'horaConsulta': dtpmInfo['horaprediccion'],
-            'id': dtpmInfo['paradero'],
-            'descripcion': dtpmInfo['nomett'],
-            'servicios': [],
-            'error': None
+            "fechaConsulta": dtpmInfo["fechaprediccion"],
+            "horaConsulta": dtpmInfo["horaprediccion"],
+            "id": dtpmInfo["paradero"],
+            "descripcion": dtpmInfo["nomett"],
+            "servicios": [],
+            "error": None
         }
 
-        if (dtpmInfo['respuestaParadero'] is not None):
-            response['error'] = dtpmInfo['respuestaParadero']
+        if (dtpmInfo["respuestaParadero"] is not None):
+            response["error"] = dtpmInfo["respuestaParadero"]
 
         # for each route
-        for route in dtpmInfo['servicios'][0]:
+        for route in dtpmInfo["servicios"][0]:
             bus_list = []
             bus1 = {
                 "servicio": None,
@@ -117,31 +117,31 @@ class WebService:
             }
 
             # information about next two buses
-            if route['codigorespuesta'] == "00":
+            if route["codigorespuesta"] == "00":
 
-                bus1['servicio'] = route['servicio'].strip()
-                bus1['patente'] = route['ppubus1'].replace("-", "").strip().upper()
-                bus1['tiempo'] = route['horaprediccionbus1']
-                bus1['distancia'] = "{} {}".format(route['distanciabus1'], ' mts.')
+                bus1["servicio"] = route["servicio"].strip()
+                bus1["patente"] = route["ppubus1"].replace("-", "").strip().upper()
+                bus1["tiempo"] = route["horaprediccionbus1"]
+                bus1["distancia"] = "{} {}".format(route["distanciabus1"], " mts.")
 
                 bus2 = {
-                    'servicio': route['servicio'].strip(),
-                    'patente': route['ppubus2'].replace("-", "").strip().upper(),
-                    'tiempo': route['horaprediccionbus2'],
-                    'distancia': "{} {}".format(route['distanciabus2'], ' mts.'),
-                    'msg': None
+                    "servicio": route["servicio"].strip(),
+                    "patente": route["ppubus2"].replace("-", "").strip().upper(),
+                    "tiempo": route["horaprediccionbus2"],
+                    "distancia": "{} {}".format(route["distanciabus2"], " mts."),
+                    "msg": None
                 }
 
                 bus_list.append(bus1)
                 bus_list.append(bus2)
 
             # information about next bus
-            elif route['codigorespuesta'] == "01":
+            elif route["codigorespuesta"] == "01":
 
-                bus1['servicio'] = route['servicio'].strip()
-                bus1['patente'] = route['ppubus1'].replace("-", "").strip().upper()
-                bus1['tiempo'] = route['horaprediccionbus1']
-                bus1['distancia'] = "{} {}".format(route['distanciabus1'], ' mts.')
+                bus1["servicio"] = route["servicio"].strip()
+                bus1["patente"] = route["ppubus1"].replace("-", "").strip().upper()
+                bus1["tiempo"] = route["horaprediccionbus1"]
+                bus1["distancia"] = "{} {}".format(route["distanciabus1"], " mts.")
 
                 bus_list.append(bus1)
 
@@ -149,22 +149,20 @@ class WebService:
             # 10: there is not buses to bus stop
             # 11: route out of schedule
             # 12: route not available
-            elif route['codigorespuesta'] in ["09", "10", "11", "12"]:
-                bus1['servicio'] = route['servicio'].strip()
-                bus1['msg'] = route['respuestaServicio']
+            elif route["codigorespuesta"] in ["09", "10", "11", "12"]:
+                bus1["servicio"] = route["servicio"].strip()
+                bus1["msg"] = route["respuestaServicio"]
 
                 bus_list.append(bus1)
 
             # 14: stop does not math with route asked
-            # 20: system error. it's catch by stop error
+            # 20: system error. it"s catch by stop error
             # 21: invalid query
             # 23: invalid stop
             # 24: invalid route. It is used when ask for route and stop, not our case
-            elif route['codigorespuesta'] in ["14", "20", "21", "23", "24"]:
-                bus1['servicio'] = route['servicio']
-                bus1['msg'] = route['respuestaServicio']
-                bus_list.append(bus1)
+            elif route["codigorespuesta"] in ["14", "20", "21", "23", "24"]:
+                print("proccessing route from authority predictor: ", route["respuestaServicio"])
 
-            response['servicios'] += bus_list
+            response["servicios"] += bus_list
 
         return response
