0a1e4aab6819034668ad2305748cc7a91cb0a73b
Felipe Hernández <cephei.1313@gmail.com>
2017-09-22 16:20:37 -0300

    refactoring

--- PredictorDTPM/models.py
+++ PredictorDTPM/models.py
@@ -1,24 +1,22 @@
 from django.db import models
 
-# Create your models here.
-
 
 class Log(models.Model):
-    """ register a call to dtpm """
+    """ register a call to authority """
     busStopCode = models.CharField(max_length=6, null=False)
     """ user code of bus stop """
     serverTimeStamp = models.DateTimeField(null=False)
-    """ server time when it received DTPM answer """
+    """ server time when it received answer from transport system authority """
     dtpmTimeStamp = models.DateTimeField(null=False)
     """ time when DTPM server made the answer """
     webTransId = models.CharField(max_length=24, null=False, db_index=True)
-    """ transaction id used to request DTPM server """
+    """ transaction id used to request transport system authority server """
     errorMessage = models.TextField(null=True)
-    """ error message returned by DTPM """
+    """ error message returned by transport system authority """
 
 
 class BusLog(models.Model):
-    """ register buses gotten from dtpm  """
+    """ register buses gotten from authority  """
     licensePlate = models.CharField(max_length=7, null=True)
     """ license plate of bus """
     route = models.CharField(max_length=7, null=True)
@@ -28,6 +26,6 @@ class BusLog(models.Model):
     distance = models.IntegerField(null=True)
     """ Distance from bus to bus stop """
     message = models.CharField(max_length=200, null=True)
-    """ message of route. It could be frecuquency, out of schedule, among others """
+    """ message of route. It could be frequency, out of schedule, among others """
     log = models.ForeignKey('Log', on_delete=models.CASCADE)
     """ Foreign key """
