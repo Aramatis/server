5c8596c5e3c3c6dc2ddf63db5a23e8171f7623a1
Felipe Hernández <cephei.1313@gmail.com>
2017-10-24 16:16:42 -0300

    added migration to fixed timeCreation field of token model

---
+++ AndroidRequests/migrations/0040_fixed_timecreation_20171024_1608.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+def fixed_timecreation(apps, schema_editor):
+    tokens = apps.get_model('AndroidRequests', 'token')
+    # trajectories = apps.get_model('AndroidRequests', 'poseintrajectoryoftoken')
+
+    for token in tokens.objects.prefetch_related('poseintrajectoryoftoken').all():
+        timeCreation = \
+        token.poseintrajectoryoftoken_set.all().order_by("-timeCreation").first().values_list("timeCreation",
+                                                                                              flat=True)[0]
+        token.timeCreation = timeCreation
+        token.save()
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('AndroidRequests', '0039_auto_20171004_1118'),
+    ]
+
+    operations = [
+        migrations.RunPython(fixed_timecreation,
+                             reverse_code=migrations.RunPython.noop),
+    ]
