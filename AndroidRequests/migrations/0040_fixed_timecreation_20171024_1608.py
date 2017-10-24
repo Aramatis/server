5b1a6da40366e5e17cfa5d421a1a54ceae14fb64
Felipe Hernández <cephei.1313@gmail.com>
2017-10-24 16:35:15 -0300

    print token updated on migration

--- AndroidRequests/migrations/0040_fixed_timecreation_20171024_1608.py
+++ AndroidRequests/migrations/0040_fixed_timecreation_20171024_1608.py
@@ -8,12 +8,15 @@ def fixed_timecreation(apps, schema_editor):
     tokens = apps.get_model('AndroidRequests', 'token')
     # trajectories = apps.get_model('AndroidRequests', 'poseintrajectoryoftoken')
 
+    counter = 0
     for token in tokens.objects.prefetch_related('poseintrajectoryoftoken_set').all():
         timeCreation = \
         token.poseintrajectoryoftoken_set.all().order_by("-timeCreation").first().values_list("timeCreation",
                                                                                               flat=True)[0]
         token.timeCreation = timeCreation
         token.save()
+        counter += 1
+        print("{0} - token {1} updated".format(counter, token.id))
 
 
 class Migration(migrations.Migration):
