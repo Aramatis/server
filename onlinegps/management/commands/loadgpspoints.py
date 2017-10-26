130709ed156ea0adad83bb7d4005dcb95f381442
Felipe Hernández <cephei.1313@gmail.com>
2017-10-26 15:25:07 -0300

    added commands to truncate table and load gps points

---
+++ onlinegps/management/commands/loadgpspoints.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.core.management.base import BaseCommand, CommandError
+from django.db import connection
+from django.conf import settings
+
+from onlinegps.models import LastGPS
+
+import time
+import os
+import gzip
+
+
+class Command(BaseCommand):
+    help = "load gps points from file (inside media folder of onlinegps app) to LastGPS model"
+
+    def add_arguments(self, parser):
+        parser.add_argument("file_name", help="file name with gps points")
+
+    def handle(self, *args, **options):
+        file_name = options["file_name"]
+        start_time = time.time()
+        table_name = "\"%s\"" % LastGPS._meta.db_table
+        try:
+            file_path = os.path.join(settings.BASE_DIR, "onlinegps", "media", file_name)
+            with gzip.open(file_path, "r") as gps_file, connection.cursor() as cursor:
+                cursor.copy_from(gps_file, table_name, ";")
+            total_time = time.time() - start_time
+            self.stdout.write(self.style.SUCCESS("copy on table %s successful: %s seconds" % (table_name, total_time)))
+        except Exception as e:
+            raise CommandError(str(e))
--- onlinegps/tests.py
+++ onlinegps/tests.py
@@ -2,5 +2,21 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.core.management import call_command
+from io import BytesIO
 
-# Create your tests here.
+
+class CommandsTest(TestCase):
+
+    def test_command_truncate_output(self):
+        """ test command truncate """
+        out = BytesIO()
+        call_command('truncate', stdout=out)
+        self.assertIn("truncate on table ", out.getvalue())
+
+    def test_command_loadgpspoints_output(self):
+        """ test command loadgpspoints """
+        out = BytesIO()
+        test_file_name = "test_ultimos_gps.csv.gz"
+        call_command("loadgpspoints", test_file_name, stdout=out)
+        self.assertIn("copy on table ", out.getvalue())
