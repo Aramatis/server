72df7375ad2b17faed6542604b3f172fec318954
Felipe Hernández <cephei.1313@gmail.com>
2017-11-20 14:58:27 -0300

    added testing for downloadgtfsdata command

--- gtfs/management/commands/downloadgtfsdata.py
+++ gtfs/management/commands/downloadgtfsdata.py
@@ -8,6 +8,7 @@ import os
 class Command(BaseCommand):
     help = 'download gtfs based on version from https://github.com/InspectorIncognito/GTFSProcessing'
 
+    downloader = urllib.URLopener()
     URL_PREFIX = 'https://raw.githubusercontent.com/InspectorIncognito/GTFSProcessing/master/procesaGTFS/x64/Release'
 
     def __init__(self):
@@ -21,21 +22,15 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('gtfs_version', help='gtfs version to load. For instance: v1.2')
+        parser.add_argument('--no-input', default=None, action='store_true',
+                            help='assume user says yes for every prompt')
 
     def handle(self, *args, **options):
         self.gtfs_version = options['gtfs_version']
+        no_input = options['no_input']
 
         try:
-            """ generate a folder with the gtfs version data """
-            FILE_NAMES = [
-                # (localName, webName)
-                ('busstop.csv', 'busstop{}.csv'.format(self.gtfs_version)),
-                ('services.csv', 'services{}.csv'.format(self.gtfs_version)),
-                ('servicesbybusstop.csv', 'servicesbybusstop{}.csv'.format(self.gtfs_version)),
-                ('servicestopdistance.csv', 'servicestopdistance{}.csv'.format(self.gtfs_version)),
-                ('servicelocation.csv', 'servicelocation{}.csv'.format(self.gtfs_version))
-            ]
-
+            """ it generates a folder with the gtfs version data """
             directory = os.path.join(self.file_path, self.gtfs_version)
             try:
                 self.stdout.write(self.style.NOTICE("creating directory '%s'" % directory))
@@ -43,26 +38,41 @@ class Command(BaseCommand):
             except OSError:
                 self.stdout.write(self.style.NOTICE("directory '%s' already exists..." % directory))
 
-            for fileName, webFileName in FILE_NAMES:
+            for fileName, webFileName in self.getFileNames():
                 filePath = os.path.join(self.file_path, self.gtfs_version, fileName)
 
                 if os.path.isfile(filePath):
                     message = "file '%s' exists. Do you want to replace it?(yes/no)" % fileName
-                    answer = raw_input(message)
+                    if no_input:
+                        answer = 'yes'
+                    else:
+                        answer = raw_input(message)
                     if answer.lower() in ['yes', 'y']:
-                        self.downloadFile(fileName, webFileName, filePath)
+                        self.download_file(fileName, webFileName, filePath)
                     else:
                         self.stdout.write(self.style.NOTICE("file '%s' skipped" % fileName))
                 else:
-                    self.downloadFile(fileName, webFileName, filePath)
+                    self.download_file(fileName, webFileName, filePath)
         except Exception as e:
             raise CommandError(str(e))
 
-    def downloadFile(self, fileName, webFileName, filePath):
+    def download_file(self, fileName, webFileName, filePath):
         """ download file from GTFSProcessing repository """
-        downloader = urllib.URLopener()
 
         url = "%s/%s" % (self.URL_PREFIX, webFileName)
         self.stdout.write(self.style.NOTICE("downloading ... %s" % url))
-        downloader.retrieve(url, filePath)
+        self.downloader.retrieve(url, filePath)
         self.stdout.write(self.style.SUCCESS("file '%s' updated" % fileName))
+
+    def getFileNames(self):
+        """  """
+        FILE_NAMES = [
+            # syntax: (localName, webName)
+            ('busstop.csv', 'busstop{}.csv'.format(self.gtfs_version)),
+            ('services.csv', 'services{}.csv'.format(self.gtfs_version)),
+            ('servicesbybusstop.csv', 'servicesbybusstop{}.csv'.format(self.gtfs_version)),
+            ('servicestopdistance.csv', 'servicestopdistance{}.csv'.format(self.gtfs_version)),
+            ('servicelocation.csv', 'servicelocation{}.csv'.format(self.gtfs_version))
+        ]
+
+        return FILE_NAMES
---
+++ gtfs/tests/tests.py
@@ -0,0 +1,118 @@
+# -*- coding: utf-8 -*-
+from django.test import TestCase, TransactionTestCase
+from django.core.management import call_command, CommandError
+from django.conf import settings
+
+from io import BytesIO
+from mock import Mock
+
+from gtfs.loaders.LoaderFactory import LoaderFactory
+from gtfs.management.commands import downloadgtfsdata
+
+import os
+import shutil
+import urllib
+
+
+class CommandLoadgtfsdataTest(TransactionTestCase):
+
+    def setUp(self):
+        self.command = 'loadgtfsdata'
+        self.gtfs_version = 'test'
+
+        self.log_file_name = 'log.log'
+        self.log_path = os.path.join(settings.BASE_DIR, 'gtfs', 'log', self.log_file_name)
+
+    def tearDown(self):
+        """ delete log file """
+        if os.path.isfile(self.log_path):
+            os.remove(self.log_path)
+
+    def test_command_loadgtfsdata_too_few_arguments(self):
+        """ test command loadgtfsdata. Raise error CommandError. too few arguments"""
+        self.assertRaises(CommandError, call_command, self.command)
+        self.assertRaises(CommandError, call_command, self.command, self.gtfs_version)
+
+    def test_command_loadgtfsdata_with_2_models_output(self):
+        """ test command loadgtfsdata. Raise error CommandError. too few arguments"""
+        out = BytesIO()
+        model = ['stop', 'route']
+        call_command(self.command, self.gtfs_version, model[0], model[1], '--logfilename',
+                     self.log_file_name, stdout=out)
+        self.assertIn(model[0], out.getvalue())
+        self.assertIn(model[1], out.getvalue())
+
+    def test_command_loadgtfsdata_output(self):
+        """ test command loadgtfsdata. Raise error CommandError. too few arguments"""
+        out = BytesIO()
+        models = ['stop', 'route', 'routelocation', 'routestopdistance', 'routebystop', 'shape']
+        for model in models:
+            call_command(self.command, self.gtfs_version, model, '--logfilename', self.log_file_name, stdout=out)
+            self.assertIn(model, out.getvalue())
+
+            if os.path.isfile(self.log_path):
+                with open(self.log_path) as log_file:
+                    self.assertNotEqual(log_file.read(), "")
+
+
+class CommandDownloadgtfsdataTest(TransactionTestCase):
+    """ test command downloadgtfsdata """
+
+    def setUp(self):
+        self.command = 'downloadgtfsdata'
+        self.gtfs_version = 'test2'
+        self.data_path = os.path.join(settings.BASE_DIR, 'gtfs', 'data', self.gtfs_version)
+
+    def tearDown(self):
+        """ delete log file """
+        if os.path.isdir(self.data_path):
+            shutil.rmtree(self.data_path)
+
+    def test_command_too_few_arguments(self):
+        """ Raise error CommandError. too few arguments"""
+        self.assertRaises(CommandError, call_command, self.command)
+        self.assertRaises(CommandError, call_command, self.command, self.gtfs_version)
+
+    def test_command_directory_does_not_exist(self):
+        """ download data """
+        out = BytesIO()
+        command_obj = downloadgtfsdata.Command()
+        urllib_mock = Mock(spec=urllib.URLopener())
+        urllib_mock.configure_mock(**{"retrieve.return_value": True})
+        command_obj.downloader = urllib_mock
+
+        call_command(command_obj, self.gtfs_version, stdout=out)
+        self.assertIn("creating directory", out.getvalue())
+        self.assertIn("file 'busstop.csv' updated", out.getvalue())
+        self.assertIn("file 'services.csv' updated", out.getvalue())
+        self.assertIn("file 'servicesbybusstop.csv' updated", out.getvalue())
+        self.assertIn("file 'servicestopdistance.csv' updated", out.getvalue())
+        self.assertIn("file 'servicelocation.csv' updated", out.getvalue())
+
+    def test_command_directory_exists(self):
+        """ download data """
+        out = BytesIO()
+        command_obj = downloadgtfsdata.Command()
+        urllib_mock = Mock(spec=urllib.URLopener())
+        urllib_mock.configure_mock(**{"retrieve.return_value": True})
+        command_obj.downloader = urllib_mock
+
+        # create directory
+        os.mkdir(self.data_path)
+        # and file exists
+        open(os.path.join(self.data_path, "busstop.csv"), 'w').close()
+
+        call_command(command_obj, self.gtfs_version, '--no-input', stdout=out)
+        self.assertIn(" already exists", out.getvalue())
+        self.assertIn("file 'busstop.csv' updated", out.getvalue())
+        self.assertIn("file 'services.csv' updated", out.getvalue())
+        self.assertIn("file 'servicesbybusstop.csv' updated", out.getvalue())
+        self.assertIn("file 'servicestopdistance.csv' updated", out.getvalue())
+        self.assertIn("file 'servicelocation.csv' updated", out.getvalue())
+
+class LoaderFactoryTest(TestCase):
+
+    def test_loader_factory_with_wrong_model(self):
+        """ tray to get loader that not exist """
+        non_exist_model = "asdad"
+        self.assertRaises(ValueError, LoaderFactory().getModelLoader, non_exist_model)
