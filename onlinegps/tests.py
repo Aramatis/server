3d19bd2965889f7c197bdeea0e8cd67d1326cacc
Felipe Hernández <cephei.1313@gmail.com>
2017-11-22 14:04:49 -0300

    added testing for is_near_to_bus_position function

--- onlinegps/tests.py
+++ onlinegps/tests.py
@@ -3,10 +3,14 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 from django.core.management import call_command
+from django.utils import timezone
+
 from io import BytesIO
 
-from onlinegps.views import get_locations, get_user_route, get_direction
+from onlinegps.views import get_locations, get_user_route, get_direction, is_near_to_bus_position
 from onlinegps.models import LastGPS
+
+from AndroidRequests.gpsFunctions import haversine
 
 
 class CommandsTest(TestCase):
@@ -27,10 +31,12 @@ class CommandsTest(TestCase):
 class VehicleDataTest(TestCase):
     """ test functions to get data related with license plate """
 
+    def setUp(self):
+        self.test_file_name = "test_ultimos_gps.csv.gz"
+
     def test_get_locations(self):
         """ test get_location function """
-        test_file_name = "test_ultimos_gps.csv.gz"
-        call_command("loadgpspoints", test_file_name)
+        call_command("loadgpspoints", self.test_file_name)
 
         license_plates = LastGPS.objects.values_list("licensePlate", flat=True)
 
@@ -57,3 +63,114 @@ class VehicleDataTest(TestCase):
 
         for index, route in enumerate(routes):
             self.assertEquals(get_user_route(route), expected_answer[index])
+
+
+class UserBusIsNearToRealBusTest(TestCase):
+    """ test functions to know if user bus is near to real bus (based on gps) """
+
+    def setUp(self):
+        self.test_file_name = "test_ultimos_gps.csv.gz"
+        call_command("loadgpspoints", self.test_file_name)
+        self.license_plate = "CJRB74"
+
+    def test_give_empty_list(self):
+        """ test is_near_to_bus_position function. It receives empty list"""
+        positions = []
+        self.assertRaises(AssertionError, is_near_to_bus_position, self.license_plate, positions)
+
+    def get_tuple(self, time_deltas, locations=None):
+
+        machine_info = get_locations([self.license_plate])[self.license_plate]
+
+        now = machine_info["timestamp"]
+        times = []
+        for time_delta in time_deltas:
+            times.append(now - timezone.timedelta(seconds=time_delta))
+
+        if locations == None:
+            locations = [
+                (-70.647482, -33.380137), # nearest point
+                (-70.64712, -33.380238),
+                (-70.646807, -33.380336),
+                (-70.64645, -33.380492)
+            ]
+
+        distance = haversine(machine_info["longitude"], machine_info["latitude"], locations[0][0], locations[0][1])
+        print("distance:", distance, " diffTime:", abs((now - times[0]).total_seconds()))
+
+        positions = [(lon, lat, tz) for (lon, lat), tz in zip(locations, times)]
+
+        return positions
+
+    def test_time_ok_but_distance_not_ok(self):
+        """ time diff 1 second but distance less than 500 meter """
+
+        time_deltas = [1, 2, 4, 6]
+
+        positions = self.get_tuple(time_deltas)
+        self.assertTrue(is_near_to_bus_position(self.license_plate, positions))
+
+        time_deltas = [-1, -2, -4, -6]
+        positions = self.get_tuple(time_deltas)
+        self.assertTrue(is_near_to_bus_position(self.license_plate, positions))
+
+    def test_time_not_ok_and_distance_not_ok(self):
+        """ time diff 30 seconds and distance less than 500 meter """
+
+        time_deltas = [30, 32, 44, 56]
+
+        positions = self.get_tuple(time_deltas)
+        self.assertTrue(is_near_to_bus_position(self.license_plate, positions))
+
+        time_deltas = [-30, -32, -44, -56]
+        positions = self.get_tuple(time_deltas)
+        self.assertTrue(is_near_to_bus_position(self.license_plate, positions))
+
+    def test_time_not_ok_and_distance_ok(self):
+        """ time diff 30 seconds but distance greater than 500 meter """
+
+        time_deltas = [30, 32, 44, 56]
+        locations = [
+            (-70.662764, -33.374385),  # nearest point
+            (-70.665184, -33.373490),
+            (-70.667759, -33.372531),
+            (-70.670559, -33.371447)
+        ]
+
+        positions = self.get_tuple(time_deltas, locations)
+        self.assertTrue(is_near_to_bus_position(self.license_plate, positions))
+
+        time_deltas = [-30, -32, -44, -56]
+        positions = self.get_tuple(time_deltas, locations)
+        self.assertTrue(is_near_to_bus_position(self.license_plate, positions))
+
+    def test_time_ok_and_distance_ok(self):
+        """ time diff 10 seconds but distance greater than 500 meter """
+
+        # negative indicate that user points are newer than bus gps
+        time_deltas = [-9, -11, -24, -36]
+        locations = [
+            (-70.662764, -33.374385),  # nearest point
+            (-70.665184, -33.373490),
+            (-70.667759, -33.372531),
+            (-70.670559, -33.371447)
+        ]
+
+        positions = self.get_tuple(time_deltas, locations)
+        self.assertFalse(is_near_to_bus_position(self.license_plate, positions))
+
+        time_deltas = [9, 11, 24, 36]
+        positions = self.get_tuple(time_deltas, locations)
+        self.assertFalse(is_near_to_bus_position(self.license_plate, positions))
+
+
+    def test_license_plate_does_not_exist(self):
+        """ license plate does not exist in database """
+
+        license_plate = "this_is_a_fake_license_plate"
+
+        # negative indicate that user points are newer than bus gps
+        time_deltas = [9, 11, 24, 36]
+
+        positions = self.get_tuple(time_deltas)
+        self.assertTrue(is_near_to_bus_position(license_plate, positions))
--- onlinegps/views.py
+++ onlinegps/views.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.utils import timezone
+
+from AndroidRequests.gpsFunctions import haversine
 
 from onlinegps.models import LastGPS
 
@@ -40,14 +43,52 @@ def get_locations(license_plate_list):
 
     answer = {}
     locations = LastGPS.objects.filter(licensePlate__in=license_plate_list).values_list('licensePlate', 'latitude',
-                                                                                        'longitude', 'userRouteCode')
+                                                                                        'longitude', 'userRouteCode',
+                                                                                        'timestamp')
 
     for location in locations:
         answer[location[0]] = {
             "latitude": location[1],
             "longitude": location[2],
             "direction": get_direction(location[3]),
-            "route": get_user_route(location[3])
+            "route": get_user_route(location[3]),
+            "timestamp": location[4]
         }
 
     return answer
+
+
+def is_near_to_bus_position(licensePlate, positions):
+    """
+    Check if time nearest in positions has distance with last gps point greater than max_distance meters
+    @return boolean -> True: i don't know anything, False: user bus is far away from real bus
+    """
+    assert len(positions) > 0, "positions is a empty list"
+
+    max_seconds = 10
+    max_distance_mts = 500
+
+    info = get_locations([licensePlate])
+
+    if licensePlate in info.keys():
+        machineInfo = get_locations([licensePlate])[licensePlate]
+    else:
+        return True
+
+    nearestPosition = None
+    diffTime = timezone.timedelta(days=700)
+
+    for index, position in enumerate(positions):
+        currentDiffTime = abs(machineInfo["timestamp"] - position[2])
+        if currentDiffTime < diffTime:
+            diffTime = currentDiffTime
+            nearestPosition = position
+
+    distance = haversine(machineInfo['longitude'], machineInfo['latitude'],
+                         nearestPosition[0], nearestPosition[1])
+    if diffTime.total_seconds() < max_seconds and distance > max_distance_mts:
+        # break trip
+        return False
+
+    # in any other case don't do anything
+    return True
