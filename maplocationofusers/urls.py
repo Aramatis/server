be4a6d06662c4acc89eda475cd27ec0e78a915fd
Felipe Hernández <cephei.1313@gmail.com>
2017-11-22 19:47:05 -0300

    added restriction to see map with users

--- maplocationofusers/urls.py
+++ maplocationofusers/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 from maplocationofusers.views import MapHandler, GetMapPositions, GetMapTrajectory, GetGamificationUsersByDay
 
 app_name = "map"
 urlpatterns = [
-    url(r'^show$', MapHandler.as_view(), name="show"),
-    url(r'^activeuserpose$', GetMapPositions.as_view(), name="activeuserpose"),
-    url(r'^activetrajectory$', GetMapTrajectory.as_view(), name="activetrajectory"),
-    url(r'^gamificatedusersbyday$', GetGamificationUsersByDay.as_view(), name="gamificatedusersbyday"),
+    url(r'^show$', login_required(MapHandler.as_view()), name="show"),
+    url(r'^activeuserpose$', login_required(GetMapPositions.as_view()), name="activeuserpose"),
+    url(r'^activetrajectory$', login_required(GetMapTrajectory.as_view()), name="activetrajectory"),
+    url(r'^gamificatedusersbyday$', login_required(GetGamificationUsersByDay.as_view()), name="gamificatedusersbyday"),
 ]
--- server/settings.py
+++ server/settings.py
@@ -276,6 +276,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+# set login url
+LOGIN_URL = "/admin/login"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
